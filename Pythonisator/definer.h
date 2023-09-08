5e83e6842e6bce2ae41c34665c5af4ba21ae05b8
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 00:01:11 +0300

    Change code to definer.h file. Delete test code.
    Oh yes, I forgot to mention that by chance the project does not include exe file.

--- Pythonisator/definer.h
+++ Pythonisator/definer.h
@@ -4,14 +4,5 @@
 #define nd &
 #define o |
 #define r |
-using namespace std;
-static void maineer()
-{
-    string f1 = "1"; string f2 = "2";
-    if (f1 == "1" and f2 == "2")
-    {
-        cout << "Hello";
-    }
-}
 
 #endif // DEFINER_H_INCLUDED
