08e4a64b52d5cdef3d315edf3ccd5ea345ab3a2b
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:10:28 +0300

    Update print.h

--- Pythonisator/print.h
+++ Pythonisator/print.h
@@ -1,6 +1,6 @@
 #ifndef PRINT_H_INCLUDED
 #define PRINT_H_INCLUDED
-#pragma message("print.h version 0.5(beta). How it works? I don't know")
+#pragma message("print.h version 0.5.1beta->Compilation completed, enjoy!")
 using namespace std;
 namespace Pythonisator_print
 {
