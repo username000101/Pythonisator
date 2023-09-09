d1f9f9ac9d5aa4f4686d59a4cae93a9887231b0a
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 15:44:21 +0300

    Сглупил, и добавил в ветвь русской версии пайтонизатора файл definer.h с английскимии комментариями. Вот, исправил

--- definer.h
+++ definer.h
@@ -1,6 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
-#define and && // Logic operator "and"
-#define or || // Logic operator "or"
+#define and && // Логический оператор "and"
+#define or || // Логический оператор "or"
 
 #endif // DEFINER_H_INCLUDED
