e9703b32bfa8fcfd3fc6649bbfc13dfb3caa4c0b
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:21:34 +0300

    Delete non usable direktory

--- Pythonisator/definer.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEFINER_H_INCLUDED
-#define DEFINER_H_INCLUDED
-#pragma message("definer.h version 0.5.1beta->Compilation completed. Enjoy!
-#define and && // Logical oprator "and"
-#define or || // Logical operator "or"
-#define not ! // Logical opeeator "not"
-#endif // DEFINER_H_INCLUDED
