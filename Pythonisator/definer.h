a0c96d25d3922c8e4ffcda463d92788740caf39f
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 10:54:21 +0300

    Add comment to definer.h
    I added comment to definer.h file.

--- Pythonisator/definer.h
+++ Pythonisator/definer.h
@@ -1,8 +1,8 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
-#define a &
-#define nd &
-#define o |
-#define r |
+#define a & // Operator "and" step 1
+#define nd & // Operator "and" step 2
+#define o | // Operator "or" step 1
+#define r | // Operator "or" step 2
 
 #endif // DEFINER_H_INCLUDED
