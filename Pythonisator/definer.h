90563f5917a0549aaa400e14606252445eb84d29
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 15:46:23 +0300

    Update definer.h
    More changes: I fixed bugs to using logical operators.

--- Pythonisator/definer.h
+++ Pythonisator/definer.h
@@ -1,8 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
-#define a & // Operator "and" step 1
-#define nd & // Operator "and" step 2
-#define o | // Operator "or" step 1
-#define r | // Operator "or" step 2
+#define and && // Logical oprator "and"
+#define or || // Logical operator "or"
 
 #endif // DEFINER_H_INCLUDED
