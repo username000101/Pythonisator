d34b7829843b19076720088bad6363daf0e7dcd7
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:23:16 +0300

    Update definer.h

--- definer.h
+++ definer.h
@@ -1,5 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
+#pragma message("definer.h version 0.5.1beta->Compilation completed. Enjoy!")
 #define and && // Logic operator "and"
 #define or || // Logic operator "or"
 #define not ! // Logic operator "not"
