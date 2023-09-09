e57391c6090d733d8d171a389efafd09b6cad777
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 15:42:58 +0300

    Add files via upload
    Updated definer.h. I  fixed error to using logic operators (and, or)

--- definer.h
+++ definer.h
@@ -1,9 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
-#pragma message("definer.h версия 0.5.1beta->Компиляция прошла успешно, удачи!")
-#define a & // Оператор "и" часть 1
-#define nd & // Оператор "и" часть 2
-#define o | // Оператор "или" часть 1
-#define r | // Оператор "или" часть 2
+#define and && // Logic operator "and"
+#define or || // Logic operator "or"
 
 #endif // DEFINER_H_INCLUDED
