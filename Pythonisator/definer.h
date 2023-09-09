8412c38f11f8a0da062888819bd2d674901cf10c
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:15:57 +0300

    define.h: Added operator not

--- Pythonisator/definer.h
+++ Pythonisator/definer.h
@@ -1,6 +1,7 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
+#pragma message("definer.h version 0.5.1beta->Compilation completed. Enjoy!
 #define and && // Logical oprator "and"
 #define or || // Logical operator "or"
-
+#define not ! // Logical opeeator "not"
 #endif // DEFINER_H_INCLUDED
