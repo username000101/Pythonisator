df35d309ff33a931c84a0942dbfb9873082c0286
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:26:09 +0300

    add pragma message

--- definer.h
+++ definer.h
@@ -1,5 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
+#pragma message("definer.h версия 0.5.1beta->Компиляция прошла успешно, удачи!")
 #define and && // Логический оператор "and"
 #define or || // Логический оператор "or"
 #define not ! // Логический оператор "not"
