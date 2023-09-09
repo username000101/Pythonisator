568e8d1330f0417d417df15b63c74ee5a3c3fdb6
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:04:45 +0300

    Незначительные изменения

--- definer.h
+++ definer.h
@@ -1,5 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
+#pragma message("definer версия 0.5.1beta->Компиляция успешна, удачи!")
 #define and && // Логический оператор "and"
 #define or || // Логический оператор "or"
 
