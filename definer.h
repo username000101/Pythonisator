347e95bd1010378fc3cd1d7bf6f9ecadf1d9955a
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 15:27:32 +0300

    Добавил сообщение при сборке в файл definer.h. Мелочь, а приятно, по крайней мере мне.

--- definer.h
+++ definer.h
@@ -1,5 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
+#pragma message("definer.h версия 0.5.1beta->Компиляция прошла успешно, удачи!")
 #define a & // Оператор "и" часть 1
 #define nd & // Оператор "и" часть 2
 #define o | // Оператор "или" часть 1
