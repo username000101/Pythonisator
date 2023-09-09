9bb6e3d51bc4d22a744a879b61369882ad465c2d
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 19:16:31 +0300

    Добавил комментарии на русском в файл filestream.h

--- filestream.h
+++ filestream.h
@@ -7,18 +7,18 @@
 using namespace std;
 namespace Pythonisator_filestream
 {
-    static void read_file(string path, string data)
+    static void read_file(string path, string data) // Метод, который принимает переменную типа string как путь до файла, и переменную типа string куда записывать данные из файла.
     {
-        ifstream file(path);
-        while(getline(file, data))
+        ifstream file(path); // Открытие потока(знаю, забыл закрыть его, в скором фиксе исправлю.
+        while(getline(file, data)) // Чтение данных из файла и запись их в переменную.
         {
-            cout << data << endl;
+            cout << data << endl; // Вывод данных.
         }
     }
-        static void write_file(string path, string data)
+        static void write_file(string path, string data) //  Метод, который работает достаточно похоже.
         {
-            ofstream file(path, ofstream::app);
-            file << data;
+            ofstream file(path, ofstream::app); // Открытие потока
+            file << data; // Запись данных, которые вы передали.
         }
 
 }
