5c13a4294f61e851948a2eb56a298f1901bad847
username000101 <144488303+username000101@users.noreply.github.com>
2023-09-09 16:07:42 +0300

    Add files via upload

--- definer.h
+++ definer.h
@@ -1,7 +1,6 @@
 #ifndef DEFINER_H_INCLUDED
 #define DEFINER_H_INCLUDED
-#pragma message("definer версия 0.5.1beta->Компиляция успешна, удачи!")
-#define and && // Логический оператор "and"
-#define or || // Логический оператор "or"
-
+#define and && // Logic operator "and"
+#define or || // Logic operator "or"
+#define not !
 #endif // DEFINER_H_INCLUDED
