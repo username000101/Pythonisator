0281199f5aae4465950c1caa530877a6682cca4f
IG <144488303+username000101@users.noreply.github.com>
2023-10-04 00:07:56 +0300

    Delete definer

--- definer.h
+++
@@ -1,9 +0,0 @@
-#pragma message("definer.h version 0.5.5beta->Compilation completed. Enjoy!")
-#ifndef DEFINER_H_INCLUDED
-#define DEFINER_H_INCLUDED
-#define input getline
-#define inputv cin>>
-#define AND && // Logic operator "and"
-#define OR || // Logic operator "or"
-#define NOT ! // Logic operator "not"
-#endif // DEFINER_H_INCLUDED
