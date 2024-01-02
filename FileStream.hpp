3b42e1025e98f3d2393f2ccf2d4e6fbf991f4425
I'm thinking <144488303+username000101@users.noreply.github.com>
2024-01-02 04:32:57 +0300

    Удалён FileStream.hpp
    Перемещён в IO.hpp

--- FileStream.hpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#define open fstream
-using namespace std;
-string read(fstream& file,signed int count);
-void write(fstream& file, string info);
